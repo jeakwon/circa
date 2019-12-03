77286b9b38a948435f5ff62fc7ad38a9c2a3663d
jeakwon <onlytojay@gmail.com>
2019-12-03 09:23:22 +0900

    circa

--- preprocessing.py
+++ preprocessing.py
@@ -4,18 +4,17 @@ import numpy as np
 import pandas as pd
 from scipy.signal import butter, filtfilt
 import utils
+import datetime
+
 class Converter:
+
     def convert(self, abf):
         fs = abf.dataRate
         converted = []
         for data in abf.data:
             data = self.__high_pass_filter(data, fs)
-            data = abs(data)
-            data = self.__down_sampling(data, fs)
+            data = self.__root_mean_square(data, fs)
         return data
-
-    def absolutize(self, abf):
-        return 
 
     @staticmethod
     def __high_pass_filter(x, fs, cutoff=1, order=5):
@@ -25,18 +24,34 @@ class Converter:
         return filtfilt(b, a, x)
 
     @staticmethod
-    def __down_sampling(x, factor):
-        M = factor
-        N = int(np.ceil(len(x)/factor))
-        
-        X = np.copy(x)
-        X.reshape(-1, M)
-        Y = np.mean(X,1)
-        
-        return Y
+    def __root_mean_square(x, window):
+        x = np.convolve(x**2, np.ones((window,))/window, mode='valid')
+        return np.sqrt(x)
+
+    @staticmethod
+    def __integral_average(x, window):
+        return np.convolve(abs(x), np.ones((window,))/window, mode='valid')
 
 
 if __name__ == '__main__':
     converter = Converter()
-    abf = pyabf.ABF('sample.abf') 
-    print(converter.convert(abf).shape)
+    abf = pyabf.ABF('sample.abf')
+    data = converter.convert(abf)
+    import matplotlib.pyplot as plt
+    # plt.plot(data)
+    # plt.show()
+    # print(data)
+    print(abf.abfDateTime)
+    df = pd.DataFrame(data)
+    # datetime.timedelta(seconds=1/20)
+    print(
+        pd.date_range(start=abf.abfDateTime, periods=len(data))
+    )
+    print(abf.dataPointCount)
+    print(len(pd.date_range(start="2018-4-1", periods=30)))
+    # from pprint import pprint
+    # for item in dir(abf):
+    #     print(item, abf.__getattribute__(item))
+    # print(abf.abfDateTime)
+    # print(abf.adcNames)
+    # # print(converter.convert(abf).shape)
